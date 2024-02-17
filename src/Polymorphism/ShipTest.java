f517c51a6a7d2b39693a23e5232c8746a96da2af
Simon Nguyen <simonnguyen99@gmail.com>
2024-02-16 23:16:40 -0800

    Changed output with no overloaded constructors.

--- src/Polymorphism/ShipTest.java
+++ src/Polymorphism/ShipTest.java
@@ -10,9 +10,23 @@ public class ShipTest {
 
         Ship[] listOfShips = new Ship[3];
 
-        listOfShips[0] = new Ship("One", "2000");
-        listOfShips[1] = new CruiseShip("Three", "2003", 1);
-        listOfShips[2] = new CargoShip("Two", "2001", 15);
+        Ship newShip = new Ship();
+        newShip.setName("SS. Ship");
+        newShip.setBuildYear("2001");
+
+        listOfShips[0] = newShip;
+
+        CruiseShip newCruiseShip = new CruiseShip();
+        newCruiseShip.setName("SS. Cruise");
+        newCruiseShip.setBuildYear("2002");
+        newCruiseShip.setMaxNumOfPassengers(200);
+        listOfShips[1] = newCruiseShip;
+
+        CargoShip newCargoShip = new CargoShip();
+        newCargoShip.setName("SS. Cargo");
+        newCargoShip.setBuildYear("2003");
+        newCargoShip.setCargoCapacityInTon(4000);
+        listOfShips[2] = newCargoShip;
 
 
         for(Ship ships : listOfShips) {
